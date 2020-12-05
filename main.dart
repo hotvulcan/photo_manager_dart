f264078f9c4b077f368d3ea21b5c4b83be0fceb5
Sooo <Sooo@sino.space>
2020-12-05 09:24:03 +0700

    出门

--- main.dart
+++ main.dart
@@ -1,23 +1,19 @@
 import 'dart:io';
-
 import 'package:exif/exif.dart';
 
 main(){
   File file = new File("example.jpg");
   printExifOf(file).then((exif) {
-    // print(exif.toString());
+    print(exif.toString());
   });
 
   
 }
 
 Future<Map<String, String>> printExifOf(File file) async {
-
   Map<String, IfdTag> data = await readExifFromBytes(await file.readAsBytes());
   Map<String, String> ret = {};
   data.forEach((key, value) { ret[key] = value.toString(); });
-  
-  ret.forEach((key, value) {print(key);});
   return ret;
   // if (data == null || data.isEmpty) {
   //   print("No EXIF information found\n");
